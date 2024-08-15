2778523926fffd454c4936d8d160615fed48e817
Sandithya21 <91774321+IT21802126-Sandithya@users.noreply.github.com>
2024-08-15 11:44:01 +0530

    Update rewardController.js

---
+++ routes/rewardRoutes.js
@@ -0,0 +1,23 @@
+import asyncHandler from 'express-async-handler';
+import Reward from '../models/rewardModel';
+import User from '../models/userModel';
+
+//User can add a review for a relevant driver
+const addReview = asyncHandler(async(req, res) => {
+    const {driverId, rating, comment} = req.body;
+    const userId = req.user._id;
+
+    const reward = await Reward.findOne({driverId});
+
+    if(reward) {
+        reward.reviews.push({userId, rating, comment});
+        reward.calculateTotalPoints();
+        await reward.save();
+        res.status(201).json({message : 'Review added successfully'});
+    }else {
+        res.status(404).json({ message: "Driver not found" });
+    }
+
+})
+
+export {addReview};
