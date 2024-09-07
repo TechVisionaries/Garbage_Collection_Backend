f312cc6e558b4f26ac5f2425a7058a2aa5c66fb2
AvishkaNuwan <it21256264@my.sliit.lk>
2024-09-07 06:03:51 +0530

    Update appointmentRoutes.js

--- routes/appointmentRoutes.js
+++ routes/appointmentRoutes.js
@@ -9,6 +9,6 @@ const router = express.Router();
 
 router.post("/", createAppointment);
 router.get("/:userId", getMyAppointments);
-router.delete("/:id", cancelAppointment);
+router.put("/:id", cancelAppointment);
 
 export default router;
