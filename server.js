df6ad09a3b1ac3abb587ae4f5d58a4674b2e74dc
Sachinthaka Ayeshmantha <it21219320@my.sliit.lk>
2024-09-15 10:29:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -24,16 +24,7 @@ app.use(cors());
 app.use(cookieParser());
 app.use(bodyParser.json());
 
-if(process.env.NODE_ENV === 'production'){
-    const __dirname = path.resolve();
-    app.use(express.static(path.join(__dirname, 'frontend/dist')));
-
-    app.get('/*', (req, res) =>
-        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
-    );
-}else{
-    app.get('/', (req, res) => res.send('Server is ready'));
-}
+app.get('/', (req, res) => res.send('Server is ready'));
 
 
 app.use('/api/users', userRoutes);
